d40c775503d11d156bed191bd2d546546e304ba5
danyuanwang <danyuanwang@hotmail.com>
2024-06-13 22:45:33 -0700

    first agent subclass

--- DefaultAgent.py
+++ DefaultAgent.py
@@ -1,6 +1,20 @@
 from Agent import Agent
-
+from tensorflow import keras
+from keras import layers
 class DefaultAgent(Agent):
+    gamma = 1
+    model = None
+    model = keras.Sequential()
+    model.add(layers.Input(shape = (3 * 3,)))
+    model.add(layers.Dense(32, activation = 'tanh'))
+    model.add(layers.Dense(32, activation = 'tanh'))
+    model.add(layers.Dense(32, activation = 'tanh'))
+    model.add(layers.Dense(32, activation = 'tanh'))
+    #model.add(layers.Dense(32, activation = 'linear'))
+    #model.add(layers.Dense(32, activation = 'linear'))
+    model.add(layers.Dense(1, activation = 'linear'))
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3),
+    loss=keras.losses.MeanSquaredError())
 
     def __init__(self, player, alpha, model):
         super(player, alpha)
