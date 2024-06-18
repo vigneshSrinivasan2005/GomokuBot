9c2163d788f18ed75e520832976ab0b3ac524996
vigneshSrinivasan2005 <Vignesh.2005.10.10@gmail.com>
2024-06-17 21:43:36 -0700

    done

--- Main.py
+++ Main.py
@@ -11,14 +11,13 @@ agent1=PlayerInput(1,.1,3)
 agent2=PlayerInput(2,.1,3)
 #call env();
 #inputs are board size, win condition, agent1, agent2, and batch size
-env=Env(3,3,agent1,agent2,2, agent1_is_training = True, agent2_is_training = True)
+env=Env(3,3,agent1,agent2,20, agent1_is_training = True, agent2_is_training = True)
 #play batches
 start= time.time()
-for i in range(3):
+for i in range(1000):
     print(i)
     env.playBatch()
 print(time.time()-start)
 #save the agents
-agent1.save("19x19_5hidden_tanh_3kbatches_20pBatch.keras")
-agent2.save("./Agent2.05.keras")
+agent1.save("PlayerInputAgent.keras")
 
