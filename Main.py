4ac54d87f5ba584baedf265ac26e74c5a6cfed05
vigneshSrinivasan2005 <Vignesh.2005.10.10@gmail.com>
2024-06-19 21:35:02 -0700

    help

--- Main.py
+++ Main.py
@@ -2,14 +2,15 @@ from Agent3x3UCB import Agent3x3UCB
 from DefaultAgent import DefaultAgent
 from Agent5x5 import Agent5x5
 from PlayerInput import PlayerInput
+from Player import Player
 from Env import Env
 import tensorflow as tf
 from tensorflow import keras
 import time
 #create 2 new agent
-#m1 = keras.models.load_model("Agent1.05.keras")
-agent1=PlayerInput(1,.1)
-agent2=PlayerInput(2,.1)
+m1 = keras.models.load_model("3x3_7hidden_tanhrelu_10kbatches_20pBatch_UCB.keras")
+agent1=Agent3x3UCB(1,.1,m1)
+agent2=Player(2)
 #call env();
 #inputs are board size, win condition, agent1, agent2, and batch size
 env=Env(3,3,agent1,agent2,20, agent1_is_training = True, agent2_is_training = True)
--- Player.py
+++ Player.py
@@ -3,6 +3,7 @@ import pygame as pg
 import sys
 import time
 from pygame.locals import *
+import numpy as np
 class Player(Agent):
 
     # to set width of the game window
@@ -34,39 +35,29 @@ class Player(Agent):
     
     # this method is used to build the
     # infrastructure of the display
-    screen = pg.display.set_mode((width, height + 100), 0, 32)
+    screen = pg.display.set_mode((width, height), 0, 32)
     
     # setting up a nametag for the
     # game window
     pg.display.set_caption("My Tic Tac Toe")
     
     # loading the images as python object
-    initiating_window = pg.image.load("modified_cover.png")
-    x_img = pg.image.load("X_modified.png")
-    y_img = pg.image.load("o_modified.png")
+    x_img = pg.image.load("./Assets/X_modified.png")
+    y_img = pg.image.load("./Assets/o_modified.png")
     
     # resizing images
-    initiating_window = pg.transform.scale(
-        initiating_window, (width, height + 100))
     x_img = pg.transform.scale(x_img, (80, 80))
     o_img = pg.transform.scale(y_img, (80, 80))
  
-    def __init__(self, player, alpha, input_model=None):
-        super().__init__(player, alpha, 3)
-        if input_model != None:
-            self.model = input_model
+    def __init__(self, player):
+        super().__init__(player, USEREVENT_DROPFILE, 3)
         Player.game_initiating_window()
         
         
     @staticmethod
     def game_initiating_window():
-    
-        # displaying over the screen
-        Player.screen.blit( Player.initiating_window, (0, 0))
-    
+        
         # updating the display
-        pg.display.update()
-        time.sleep(3)
         Player.screen.fill( Player.white)
     
         # drawing vertical lines
@@ -78,102 +69,58 @@ class Player(Agent):
         pg.draw.line( Player.screen,  Player.line_color, (0,  Player.height / 3), ( Player.width,  Player.height / 3), 7)
         pg.draw.line( Player.screen,  Player.line_color, (0,  Player.height / 3 * 2),
                     ( Player.width,  Player.height / 3 * 2), 7)
+        pg.display.update()
+
         
     
-    @staticmethod
-    def drawXO(row, col):
-        global board, XO
-    
-        # for the first row, the image
-        # should be pasted at a x coordinate
-        # of 30 from the left margin
-        if row == 1:
-            posx = 30
-    
-        # for the second row, the image
-        # should be pasted at a x coordinate
-        # of 30 from the game line
-        if row == 2:
-    
-            # margin or width / 3 + 30 from
-            # the left margin of the window
-            posx = width / 3 + 30
-    
-        if row == 3:
-            posx = width / 3 * 2 + 30
-    
-        if col == 1:
-            posy = 30
-    
-        if col == 2:
-            posy = height / 3 + 30
-    
-        if col == 3:
-            posy = height / 3 * 2 + 30
-    
-        # setting up the required board
-        # value to display
-        board[row-1][col-1] = XO
-    
-        if(XO == 'x'):
-    
-            # pasting x_img over the screen
-            # at a coordinate position of
-            # (pos_y, posx) defined in the
-            # above code
-            screen.blit(x_img, (posy, posx))
-            XO = 'o'
-    
-        else:
-            screen.blit(o_img, (posy, posx))
-            XO = 'x'
+    def drawBoard(state):
+        Player.game_initiating_window()
+        rows=len(state)
+        cols=len(state[0])
+        for row in range(rows):
+            for col in range(cols):
+                if state[row][col] != 0:
+                    posx= 30 + Player.width / cols * (col)
+                    posy = 30 + Player.height / rows * (row)
+                    XO= 'x' if state[row][col]==1 else 'o'
+                    if(XO == 'x'):
+                        # pasting x_img over the screen
+                        # at a coordinate position of
+                        # (pos_y, posx) defined in the
+                        # above code
+                        Player.screen.blit(Player.x_img, (posx, posy))    
+                    else:
+                        Player.screen.blit(Player.o_img, (posx, posy))
         pg.display.update()
     
     
-    def user_click():
+    def user_click(board,player):
         # get coordinates of mouse click
         x, y = pg.mouse.get_pos()
-    
-        # get column of mouse click (1-3)
-        if(x < width / 3):
-            col = 1
-    
-        elif (x < width / 3 * 2):
-            col = 2
-    
-        elif(x < width):
-            col = 3
-    
-        else:
-            col = None
-    
-        # get row of mouse click (1-3)
-        if(y < height / 3):
-            row = 1
-    
-        elif (y < height / 3 * 2):
-            row = 2
-    
-        elif(y < height):
-            row = 3
-    
-        else:
-            row = None
-    
-        # after getting the row and col,
-        # we need to draw the images at
-        # the desired positions
-        if(row and col and board[row-1][col-1] is None):
-            global XO
-    
-            drawXO(row, col)
-            check_win()
-    
+        boardSize=len(board)
+
+        col= int(x // (Player.width / boardSize))
+        row= int(y // (Player.height / boardSize))
+        # check if full
+        if board[row][col] != 0:
+            return None
+        move=np.zeros((boardSize,boardSize)).tolist()
+        move[row][col]=player
+        return move
 
     def getMove(self, state, epsilon):
 
+        state=super()._toArray(state)
+        Player.drawBoard(state)
+        pg.display.update()
 
-        return best_move
+        move=None
+        while move is None:
+            if pg.event.get(MOUSEBUTTONDOWN):
+                move=Player.user_click(state,self.player,)
+                if move is not None:
+                    move=super()._toInt(Player.user_click(state,self.player,))
+        return move
 
     def update(self):
         return 
