525f81576e31111c1f23606079858ab2a84965d8
danyuanwang <danyuanwang@hotmail.com>
2024-06-19 18:56:42 -0700

    playerinpputed

--- Main.py
+++ Main.py
@@ -8,14 +8,14 @@ from tensorflow import keras
 import time
 #create 2 new agent
 #m1 = keras.models.load_model("Agent1.05.keras")
-agent1=PlayerInput(1,.1,3)
-agent2=PlayerInput(2,.1,3)
+agent1=PlayerInput(1,.1)
+agent2=PlayerInput(2,.1)
 #call env();
 #inputs are board size, win condition, agent1, agent2, and batch size
 env=Env(3,3,agent1,agent2,20, agent1_is_training = True, agent2_is_training = True)
 #play batches
 start= time.time()
-for i in range(1000):
+for i in range(10):
     print(i)
     start2= time.time()
     env.playBatch()
--- PlayerInput.py
+++ PlayerInput.py
@@ -9,7 +9,7 @@ class PlayerInput(Agent):
     gamma = 1
     model = None
     model = keras.Sequential()
-    model.add(layers.Input(shape = (10,)))
+    model.add(layers.Input(shape = ((3*3) +1,)))
     model.add(layers.Dense(32, activation = 'tanh'))
     model.add(layers.Dense(32, activation = 'tanh'))
     model.add(layers.Dense(32, activation = 'tanh'))
@@ -19,97 +19,32 @@ class PlayerInput(Agent):
     model.add(layers.Dense(1, activation = 'linear'))
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3),
     loss=keras.losses.MeanSquaredError())
-    def __toArray(self, input):
-        array = []
-        temp = input
-        for i in range(self.board_size):
-            row = []
-            for j in range(self.board_size):
-                row.append(temp % 3)
-                temp = temp//3
-            array.append(row)
-        return array
-    def __getStateValue(self, state):
-        state = self.__toArray(state)
+
+    def __init__(self, player, alpha, input_model=None):
+        super().__init__(player, alpha, 3)
+        if input_model != None:
+            self.model = input_model
+    
+    def _getStateValue(self, state):
+        state = self._toArray(state)
         if(state == None):
             return 0
         input = np.array(state).flatten()
-        input= np.insert(input, 0, self.player-1)
+        input= np.insert(input, 0, self.player)
         input = [input]
         input = np.array(input)
         out = self.model(input, training = False)
         return out[0]
-    def updateBatch(self, reward):
-        #print(self.batch)
-        #print("sad ", state, " ", next_state)
-        if(self.this_state_action == None):
-            y = reward
-            x = self.last_state_action
+    
 
-            if not (x in self.batch["state"].values):
-                self.batch.loc[len(self.batch.index)] = [x, 0.0]
-
-            #TODO is bugged
-            #Takes the value of the state_action_pair x', in the batch, and updates it to be equal to x' + alpha *(y - x') 
-            # where y is equal to the value estimate given by the current model estimate of the next state action pair and the reward
-            self.batch.loc[self.batch["state"] == x, "value"] += self.alpha * (y - self.batch.loc[self.batch["state"] == x, "value"].iloc[0])
-
-
-        elif(self.last_state_action != None):
-            #print(state)
-            y = reward + self.gamma * tf.get_static_value(self.__getStateValue(self.this_state_action))[0]
-            #print(tf.get_static_value(y)[0], " y")
-            x = self.last_state_action
-
-            if not (x in self.batch["state"].values):
-                self.batch.loc[len(self.batch.index)] = [x, 0.0]
-            #TODO is bugged
-            self.batch.loc[self.batch["state"] == x, "value"] += (self.alpha * (y - self.batch.loc[self.batch["state"] == x, "value"].iloc[0]))
-        #print(self.batch)
-        #print("done w/", self.player)
-    def __getLegalMoves(self, state):
-        cur_state = state
-        legal_moves = []
-        move = 1
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                if(cur_state % 3 == 0):
-                    legal_moves.append(move*self.player)
-                cur_state = cur_state // 3
-                move *= 3
-        return legal_moves
-    def __updateAgent(self, new_state_action):
-        self.last_state_action = self.this_state_action
-        self.this_state_action = new_state_action
-
-    def __getNextState(self, state, move):
-        #print(str(move))
-        return state + move
-    def __init__(self, player, alpha, board_size, input_model=None):
-        super().__init__(player, alpha, 3)
-        if input_model != None:
-            self.model = input_model
-        self.board_size=board_size
-    def getMove(self, state, epsilon):
-        legal_moves = self.__getLegalMoves(state)
-        best_move = None
-        best_score = -1000000
-        for move in legal_moves:
-            next = self.__getNextState(state, move)
-            value = self.__getStateValue(next)
-            if(value >= best_score):
-                best_move = move
-                best_score = value
-        if (random.random() < epsilon):
-            print("random")
-            best_move = random.choice(legal_moves)
-        self.__updateAgent(self.__getNextState(state, best_move))
-        return best_move
+    
     def update(self):
         x = np.array(self.batch["state"].to_list())
-        new_x = [self.player-1]
+        new_x = []
         for value in x:
-            new_x.append(np.array(self.__toArray(value)).flatten())
+            temp = np.array(self._toArray(value)).flatten()
+            temp = np.insert(temp, 0, self.player)
+            new_x.append(temp)
         new_x = np.array(new_x)
         #print(x, " x")
         y = np.array(self.batch["value"].to_list())
