9acbabaa14f30cfbcc1c5cce5685d958829f6151
vigneshSrinivasan2005 <Vignesh.2005.10.10@gmail.com>
2024-06-28 13:05:19 -0700

    added the mean squared error func and print

--- Main.py
+++ Main.py
@@ -1,24 +1,23 @@
-from MinimaxModel import MinimaxModel
+from DefaultMinimaxAgent import DefaultMiniMaxAgent
 from DefaultAgent import DefaultAgent
-from Player import Player
 from Env import Env
 import tensorflow as tf
-from tensorflow import keras
 import time
 #create 2 new agent)
-model = keras.models.load_model("3x3model.keras")
-agent1 = DefaultAgent(1, 0.1, input_model=model)
-agent2 = Player(2)
+agent1 = DefaultMiniMaxAgent(1, 0.1)
+agent2 = DefaultMiniMaxAgent(2,0.1)
 #call env();
 #inputs are board size, win condition, agent1, agent2, and batch size
-env=Env(3,3,agent1,agent2,1,epsilon=0, agent1_is_training = False, agent2_is_training = False)
+env=Env(3,3,agent1,agent2,1,epsilon=0, agent1_is_training = True, agent2_is_training = True)
 #play batches
 start= time.time()
-for i in range(1):
+for i in range(100):
     print(i)
     start2= time.time()
     env.playBatch()
+    #print("Error: ",agent1.calculateMeanSquareError())
     print(time.time()-start2)
 print(time.time()-start)
 #save the agents
-agent1.save("3x3model.keras")
+#agent1.savePlict("3x3MiniMaxPlict.csv")
+agent1.save("DefaultMiniMaxAgent.keras")
--- MiniMaxAgent.py
+++ MiniMaxAgent.py
@@ -2,8 +2,8 @@ from Agent import Agent
 import pandas as pd
 import random
 class MiniMaxAgent(Agent):
-    
     plict={}
+    miniMax=pd.read_csv("modifiedMinMax.csv")
     def __init__(self,player,alpha,board_size):
         super().__init__(player,alpha,board_size)
         pass
@@ -13,21 +13,38 @@ class MiniMaxAgent(Agent):
         best_score = -1000000 if self.player == 1 else 1000000
         for move in legal_moves:
             next = self._getNextState(state, move)
-            value = self._getStateValue(next) * (1 if self.player == 1 else -1)
-            if(value >= best_score):
-                best_move = move
-                best_score = value
+            value = self._getStateValue(next)
+            if(self.player == 1):
+                if(value >= best_score):
+                    best_move = move
+                    best_score = value
+            else:
+                if(value <= best_score):
+                    best_move = move
+                    best_score = value
+
+            
         if (random.random() < epsilon):
-            #print("random")
+            print("random")
             best_move = random.choice(legal_moves)
         nextState = self._getNextState(state, best_move)
+        #arrayState = self._toArray(nextState)
+        #print(arrayState[0])
+        #print(arrayState[1])
+        #print(arrayState[2])
+        #print()
         #dicts cannot store duplicates
         self.plict[nextState]=best_score*(1 if self.player == 1 else -1)
         self._updateAgent(nextState)
 
         return best_move
-    def savePlict(self):
+    def savePlict(self,name="plict.csv"):
         df= pd.DataFrame(columns=["State","Value"])
         for key in self.plict:
             df.loc[len(df)]=[key,self.plict[key]]
-        df.to_csv("plict.csv",index=False)
+        df.to_csv(name,index=False)
+    def calculateMeanSquareError(self):
+        error=0
+        for key in self.plict:
+            error+=(self.plict[key]-(self.miniMax[self.miniMax["State"]==key].values[0][1]/10))**2
+        return error
