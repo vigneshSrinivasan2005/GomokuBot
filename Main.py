b013649383086883391b5f87a08f403e4a57a5c8
vigneshSrinivasan2005 <Vignesh.2005.10.10@gmail.com>
2024-07-05 20:08:13 -0700

    yes

--- Main.py
+++ Main.py
@@ -8,17 +8,17 @@ from tensorflow import keras
 import time
 #create 2 new agent)
 #model = keras.models.load_model("3x3model.keras")
-agent1 = MinimaxModel(3,"modifiedMinMax.csv",1)
-agent1.save("OPModel.keras")
-agent1 = DefaultMiniMaxAgent(1, 0.01, input_model = agent1.model)
-agent2 = DefaultMiniMaxAgent(2, 0.01, input_model = agent1.model)
+#agent1 = MinimaxModel(3,"modifiedMinMax.csv",1)
+#agent1.save("OPModel.keras")
+agent1 = DefaultMiniMaxAgent(1, 0.01)
+agent2 = DefaultMiniMaxAgent(2, 0.01)
 #call env();
 #inputs are board size, win condition, agent1, agent2, and batch size
-env=Env(3,3,agent1,agent2,1,epsilon=0, agent1_is_training = True, agent2_is_training = True)
+env=Env(3,3,agent1,agent2,20,epsilon=0, agent1_is_training = True, agent2_is_training = True)
 #play batches
 
 start= time.time()
-for i in range(100):
+for i in range(10):
     print(i)
     start2= time.time()
     env.playBatch()
--- MiniMaxAgent.py
+++ MiniMaxAgent.py
@@ -1,9 +1,11 @@
 from Agent import Agent
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 import random
 class MiniMaxAgent(Agent):
     minimax= pd.read_csv("modifiedMinMax.csv")
+    batch = pd.DataFrame(columns = ["state", "value", "count"])
     def __init__(self,player,alpha,board_size):
         super().__init__(player,alpha,board_size)
         pass
@@ -30,6 +32,7 @@ class MiniMaxAgent(Agent):
     def _updateAgent(self, state, move):
         self.last_state_action = state
         self.this_state_action = state + move
+
     def evaluate(self):
         x = np.array(self.minimax["State"].to_list())
         new_x = []
@@ -39,4 +42,55 @@ class MiniMaxAgent(Agent):
         #print(x, " x")
         y = np.array(self.minimax["Value"].to_list())
         self.model.evaluate(new_x,y)
-    
+
+    def updateBatch(self, reward):
+        print(MiniMaxAgent.batch)
+        #print("sad ", state, " ", next_state)
+        if self.last_state_action != None:
+            last_state_prediction = tf.get_static_value(self._getStateValue(self.last_state_action))[0]
+            this_state_prediction = tf.get_static_value(self._getStateValue(self.this_state_action))[0]
+            
+            y= last_state_prediction+self.alpha * (reward + self.gamma  * this_state_prediction-last_state_prediction) 
+
+            x = self.last_state_action
+            if not (x in MiniMaxAgent.batch["state"].values):
+                MiniMaxAgent.batch.loc[len(MiniMaxAgent.batch.index)] = [x, y,1]
+            else:
+                MiniMaxAgent.batch.loc[MiniMaxAgent.batch["state"] == x, "value"] += y
+                MiniMaxAgent.batch.loc[MiniMaxAgent.batch["state"] == x, "count"] += 1
+        #is a terminal state so value is just the reward
+        if(self.this_state_action == None):
+            y = reward
+            x = self.last_state_action
+
+            if not (x in MiniMaxAgent.batch["state"].values):
+                MiniMaxAgent.batch.loc[len(MiniMaxAgent.batch.index)] = [x, y,1]
+            else:
+                #TODO is bugged
+                #Takes the value of the state_action_pair x', in the batch, and updates it to be equal to x' + alpha *(y - x') 
+                # where y is equal to the value estimate given by the current model estimate of the next state action pair and the reward
+                MiniMaxAgent.batch.loc[MiniMaxAgent.batch["state"] == x, "value"] += y
+                MiniMaxAgent.batch.loc[MiniMaxAgent.batch["state"] == x, "count"] += 1
+    def avgBatch(self):
+        new_batch = pd.DataFrame(columns = ["state", "value"])
+        for index, row in MiniMaxAgent.batch.iterrows():
+            new_batch.loc[index, "state"] = row["state"]
+            new_batch.loc[index, "value"] = row["value"]/row["count"]
+        print(new_batch)
+        MiniMaxAgent.batch = new_batch
+    def update(self):
+        if(MiniMaxAgent.batch.empty):
+            return
+        self.avgBatch()
+        print(MiniMaxAgent.batch)
+        x = np.array(MiniMaxAgent.batch["state"].to_list())
+        new_x = []
+        for value in x:
+            new_x.append(np.array(self._toArray(value)).flatten())
+        new_x = np.array(new_x)
+        #print(x, " x")
+        y = np.array(MiniMaxAgent.batch["value"].to_list())
+        #print(y, " y")
+        self.model.train_on_batch(new_x, y)
+        #print("emptying batch")
+        MiniMaxAgent.batch = pd.DataFrame(columns = ["state", "value","count"])
